2b3e5d74a26d21716ec5b8d2cf0701384d7bd8f4
Locke <moyerock@gmail.com>
2016-05-25 23:36:58 +0800

    feat: add sample

--- src/serverSide/testCombineLatest.js
+++ src/serverSide/testCombineLatest.js
@@ -1,15 +1,19 @@
 var Rx = require('rx')
+var SPEED = 1000
+var AMOUNT = 100
 
 var source1 = Rx.Observable.interval(100)
   .map(i => 'First: ' + i)
 
 var source2 = Rx.Observable.interval(150)
-  .map(i => 'Second: '  + i)
+  .map(i => 'Second: ' + i)
 
 var source = Rx.Observable.combineLatest(
   source1,
   source2
-).take(4)
+  )
+  .take(AMOUNT)
+  .sample(SPEED)
 
 source.subscribe(
   x => console.log('Next %s', JSON.stringify(x)),
