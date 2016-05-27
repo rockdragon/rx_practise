4873fedc73f222eef30d06d19eaaa8745bb61aea
Locke <moyerock@gmail.com>
2016-05-27 16:51:55 +0800

    feat: replay

--- src/serverSide/testReplay.js
+++ src/serverSide/testReplay.js
@@ -4,16 +4,8 @@ var source = Rx.Observable
   .interval(1000)
   .do(x => console.log('Side Effect: ', x))
 
-var a = Rx.Observable.interval(200).map((i) => {
-  return 'A' + i
-})
-
-var b = Rx.Observable.interval(200).map((i) => {
-  return 'B' + i
-})
-
 var published = source.replay(
-  x => Rx.Observable.merge(a, b).repeat(2)
+  x => x.take(3).repeat(3)
   , 2)
 
 published.subscribe(createObserver('SourceA'));
@@ -22,7 +14,7 @@ published.subscribe(createObserver('SourceB'));
 function createObserver(tag) {
   return Rx.Observer.create(
     function (x) {
-      console.log('Next: ' + tag + ': ' + x);
+      console.log('Next: ' + tag + ': ' + x.toString());
     },
     function (err) {
       console.log('Error: ' + err);
