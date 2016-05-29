a36ad9de29a5634b501c40a7e9ef7a74dbf5757a
Locke <moyerock@gmail.com>
2016-05-29 13:37:48 +0800

    feat: integrate Cycle.js with es2015 & JSX through babel

--- src/web/cycle_wiki_search/index.js
+++ src/web/cycle_wiki_search/index.js
@@ -1,11 +1,10 @@
-import Cycle from '@cycle/core';
-import {makeDOMDriver, hJSX} from '@cycle/dom';
+import Cycle from '@cycle/core'
+import {makeDOMDriver, hJSX} from '@cycle/dom'
+import Rx from 'rx'
 
-var Rx = require('rx');
-
-var MAIN_URL = 'https://en.wikipedia.org';
-var WIKI_URL = MAIN_URL + '/wiki/';
-var API_URL = MAIN_URL + '/w/api.php?action=query&list=search&format=json&srsearch=';
+const MAIN_URL = 'https://en.wikipedia.org'
+const WIKI_URL = MAIN_URL + '/wiki/'
+const API_URL = MAIN_URL + '/w/api.php?action=query&list=search&format=json&srsearch='
 
 function main(responses) {
   return {
@@ -17,19 +16,19 @@ function main(responses) {
 
 var drivers = {
   DOM: makeDOMDriver('#container')
-};
+}
 
 function vtreeElementsJSX(results) {
   results = results.map(function(result) {
     var link = WIKI_URL + result.title;
     return <div><a href={link}>{result.title}</a></div>
-  });
+  })
   return <div>
     <h1>Wikipedia Search</h1>
     <input className="search-field" type="text" />
     <hr/>
     <div>{results}</div>
-  </div>;
+  </div>
 }
 
 function searchRequest(responses) {
@@ -38,8 +37,8 @@ function searchRequest(responses) {
     .debounce(300)
     .map(e => e.target.value)
     .filter(value => value.length > 2)
-    .map(search => API_URL + search);
+    .map(search => API_URL + search)
 }
 
-Cycle.run(main, drivers);
+Cycle.run(main, drivers)
 
