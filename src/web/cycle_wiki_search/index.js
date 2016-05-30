f3374028ac4bb4dba7986098d2fbb7221e411361
Locke <moyerock@gmail.com>
2016-05-30 11:28:55 +0800

    refactor: a kind of cycle

--- src/web/cycle_wiki_search/index.js
+++ src/web/cycle_wiki_search/index.js
@@ -1,16 +1,29 @@
 import Cycle from '@cycle/core'
-import {makeDOMDriver, hJSX} from '@cycle/dom'
+import {makeDOMDriver, hJSX, div, input, p} from '@cycle/dom'
 import Rx from 'rx'
 
 const MAIN_URL = 'https://en.wikipedia.org'
 const WIKI_URL = MAIN_URL + '/wiki/'
 const API_URL = MAIN_URL + '/w/api.php?action=query&list=search&format=json&srsearch='
 
-function main(responses) {
+function get(id) {
+  return document.getElementById(id)
+}
+
+function main(drivers) {
   return {
-    DOM: Rx.Observable.just(
-      <span>hi, there!</span>
-    )
+    DOM: drivers.DOM.select('input[type="radio"]').events('click')
+      .map(ev => ev.target.value)
+      .startWith('')
+      .map(selected =>
+        <div>
+          <form>
+            <input type="radio" name="radios" value="1"/> 1
+            <input type="radio" name="radios" value="2"/> 2
+          </form>
+          <p>{selected}</p>
+        </div>
+      )
   }
 }
 
@@ -19,13 +32,13 @@ var drivers = {
 }
 
 function vtreeElementsJSX(results) {
-  results = results.map(function(result) {
+  results = results.map(function (result) {
     var link = WIKI_URL + result.title;
     return <div><a href={link}>{result.title}</a></div>
   })
   return <div>
     <h1>Wikipedia Search</h1>
-    <input className="search-field" type="text" />
+    <input className="search-field" type="text"/>
     <hr/>
     <div>{results}</div>
   </div>
