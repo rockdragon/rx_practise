52dcc939eef199764cc7c2e2bd3f43ae2f26ada9
Locke <moyerock@gmail.com>
2016-05-31 12:24:14 +0800

    feat: MVI

--- src/web/cycle_wiki_search/index.js
+++ src/web/cycle_wiki_search/index.js
@@ -1,4 +1,5 @@
 import Cycle from '@cycle/core'
+import CycleJSONP from '@cycle/jsonp'
 import {makeDOMDriver, hJSX, div, input, p} from '@cycle/dom'
 import Rx from 'rx'
 
@@ -6,50 +7,48 @@ const MAIN_URL = 'https://en.wikipedia.org'
 const WIKI_URL = MAIN_URL + '/wiki/'
 const API_URL = MAIN_URL + '/w/api.php?action=query&list=search&format=json&srsearch='
 
-function get(id) {
-  return document.getElementById(id)
+function intent(JSONP) {
+  return JSONP.filter(res$ => res$.request.indexOf(API_URL) === 0)
+    .concatAll()
+    .pluck('query', 'search')
 }
 
-function main(drivers) {
-  return {
-    DOM: drivers.DOM.select('input#search').events('input')
-      .debounce(200)
-      .map(ev => ev.target.value)
-      .startWith('')
-      .map(text =>
-        <div>
-          <input type="text" id="search" />
-          <p>you've typed: {text}</p>
-        </div>
-      )
-  }
+function model(actions) {
+  return actions.startWith([])
 }
 
-var drivers = {
-  DOM: makeDOMDriver('#container')
-}
-
-function vtreeElementsJSX(results) {
-  results = results.map(function (result) {
-    var link = WIKI_URL + result.title;
-    return <div><a href={link}>{result.title}</a></div>
+function view(state) {
+  return state.map(function(linkArray) {
+    linkArray = linkArray.map(result => {
+      var link = WIKI_URL + result.title;
+      return <div><a href={link}>{result.title}</a></div>
+    });
+    return <div>
+      <h1>Wikipedia Search</h1>
+      <input className="search-field" type="text" />
+      <hr/>
+      <div>{linkArray}</div>
+    </div>
   })
-  return <div>
-    <h1>Wikipedia Search</h1>
-    <input className="search-field" type="text"/>
-    <hr/>
-    <div>{results}</div>
-  </div>
 }
 
-function searchRequest(responses) {
-  return responses.DOM.select('.search-field')
-    .events('input')
+function userIntent(DOM) {
+  return DOM.select('.search-field').events('input')
     .debounce(300)
-    .map(e => e.target.value)
-    .filter(value => value.length > 2)
-    .map(search => API_URL + search)
+    .map(function(e) { return e.target.value })
+    .filter(function(value) { return value.length > 2 })
+    .map(function(search) { return API_URL + search });
 }
 
-Cycle.run(main, drivers)
+function main(responses) {
+  return {
+    DOM: view(model(intent(responses.JSONP))),
+    JSONP: userIntent(responses.DOM)
+  };
+}
+
+Cycle.run(main, {
+  DOM: makeDOMDriver('#container'),
+  JSONP: CycleJSONP.makeJSONPDriver()
+})
 
