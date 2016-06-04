4a112e3c9f11caab4719c9449208e21d98acb9d6
Locke <moyerock@gmail.com>
2016-06-04 16:00:09 +0800

    feat: holy crap

--- src/web/douban_api/index.js
+++ src/web/douban_api/index.js
@@ -2,55 +2,86 @@ import Cycle from '@cycle/core'
 import CycleJSONP from '@cycle/jsonp'
 import {makeDOMDriver, hJSX} from '@cycle/dom'
 import {makeHTTPDriver} from '@cycle/http'
+import {makeJSONPDriver} from '@cycle/jsonp'
 import Rx from 'rx'
 
-const HELLO_URL = 'https://www.baidu.com/s?wd='
+const GITHUB_SEARCH_URL = 'https://api.github.com/search/repositories?q='
 
 function model(actions$) {
   return Rx.Observable
-    .merge(actions$.inputs$, actions$.clicks$)
+    .merge(actions$.text$, actions$.button$, actions$.http$)
     .startWith({
-      keyword: '', request: {
-        url: HELLO_URL,
-        method: 'GET'
-      }
+      keyword: '',
+      request: null,
+      response: 'Loading...'
     })
     .scan((prev, next) => {
+      console.log('trigged scan...', next)
       const state = Object.assign({}, prev)
-      if (typeof next === 'string') {
-        state.keyword = next
-        state.request = undefined
-      }
-      else if (typeof next === 'function')
+      if (typeof next === 'function') {
+        console.log('keyword', state.keyword)
         state.request = next(state.keyword)
+      } else if (next.keyword) {
+        state.keyword = next.keyword
+        state.request = null
+      } else if (next.response) {
+        state.request = null
+        state.response = next.response
+      }
+
       return state
     })
 }
 function view(state$) {
+  console.log('---->>>>', state$.pluck('request'))
   return {
-    DOM: state$.map((value) => {
-        return <div>
-          <input type="text"/>
-          <input type="button" value="search"/>
-          <br/>
-        </div>
-      }
-    ),
-    HTTP: state$.map(value => value.request)
-  }
-}
-function intent(sources$) {
-  return {
-    inputs$: sources$.DOM.select('input[type="text"]')
-      .events('input')
-      .map(ev => ev.target.value),
-    clicks$: sources$.DOM.select('input[type="button"]')
-      .events('click')
-      .map(_ => keyword => {
+    DOM: state$
+      .filter(state => {
+        console.log('DOM')
+        return state.request === null
+      })
+      .map(state => {
+          return <div>
+            <input type="text"/>
+            <input type="button" value="search"/>
+            <br/>
+          <span>
+            {state.response}
+          </span>
+          </div>
+        }
+      ),
+    HTTP: state$
+      .filter(state => {
+        console.log('HTTP')
+        return state.request !== null
+      })
+      .map(state => {
         return {
-          url: HELLO_URL + keyword,
+          url: state.request,
           method: 'GET'
         }
+      })
+      .take(1)
+  }
+}
+function intent(responses$) {
+  return {
+    text$: responses$.DOM.select('input[type="text"]')
+      .events('input')
+      .map(e => {
+        return {keyword: e.target.value}
+      }),
+    button$: responses$.DOM.select('input[type="button"]')
+      .events('click')
+      .map(_ => keyword => GITHUB_SEARCH_URL + encodeURI(keyword)),
+    http$: responses$.HTTP
+      .filter(res$ => {
+        return res$.request.url && res$.request.url.startsWith(GITHUB_SEARCH_URL)
+      })
+      .mergeAll()
+      .map(res => {
+        return {response: res.text}
       })
   }
 }
@@ -62,6 +93,7 @@ function main(responses$) {
 const driver = {
   DOM: makeDOMDriver('#container'),
   HTTP: makeHTTPDriver(),
+  //JSONP: makeJSONPDriver(),
 }
 
 Cycle.run(main, driver)
